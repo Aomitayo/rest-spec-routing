e8e6208c8fec830fffad06c7ea522e6636c6abd8
Adedayo Omitayo <adedayo.omitayo@flowswift.com>
2014-10-20 16:46:06 +0100

    Implememted file paramters in swagger

--- tests/spec/routing.js
+++ tests/spec/routing.js
@@ -2,14 +2,19 @@
 
 /*global describe, it, before */
 
+var fs = require('fs');
 var expect = require('chai').expect;
 var supertest = require('supertest');
 var routing = require(__dirname + '/../../');
 
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var multipart = require('connect-multiparty');
+
 describe('rest spec routing', function(){
 	it('Creates routes from swagger spec', function(){
 		var router = routing.useSpec('swagger', {specDir: __dirname + '/../fixtures/swagger'});
-		expect(router.stack).to.have.length(4);
+		expect(router.stack).to.have.length(5);
 	});
 });
 
@@ -26,7 +31,20 @@ describe('rest spec commands and callbacks', function(){
 			cb(null, 'hello ' + command.params.person);
 		});
 
+		router.hooks.register('helloPersonData', function(command, cb){
+			if(command.params.hellodata){
+				cb(null, 'hello ' + fs.readFileSync(command.params.hellodata.path, 'utf8'));
+			}
+			else{
+				cb(null, {error:'Invalid data'}, {status:400});
+			}
+		});
+
 		app = require('express')();
+		app.use(bodyParser.urlencoded({extended:true}));
+		app.use(bodyParser.json({type:['*/json']}));
+		app.use(multipart());
+		app.use(cookieParser());
 		app.use(router);
 	});	
 
@@ -46,6 +64,26 @@ describe('rest spec commands and callbacks', function(){
 		.expect(200)
 		.end(function(err, res){
 			expect(res.text).to.equal('hello john');
+			done(err);
+		});
+	});
+
+	it('routes http file parameters', function(done){
+		supertest(app)
+		.post('/hello/john')
+		.attach('hellodata', __dirname +'/../fixtures/textfile.txt')
+		.expect(200)
+		.end(function(err, res){
+			expect(res.text).to.equal('hello john with data');
+			done(err);
+		});
+	});
+	it('routes http file parameters to undefined', function(done){
+		supertest(app)
+		.post('/hello/john')
+		.expect(400)
+		.end(function(err, res){
+			expect(res.body.error).to.equal('Invalid data');
 			done(err);
 		});
 	});
